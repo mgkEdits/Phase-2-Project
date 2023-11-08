c8a1ba20b7c21cffea5093c76dd2683e58a5b80a
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-11-08 22:22:24 +0300

    about us & contact us Pages

--- mgk_creates/src/components/AboutUs.js
+++ mgk_creates/src/components/AboutUs.js
@@ -2,6 +2,46 @@ import React from 'react'
 
 export default function AboutUs() {
   return (
-    <div>League</div>
+    <section classNmae="about section" id="about">
+    <div className="container">
+      <div className="section-header">
+        <h3 className="title" data-title="Who Am I">About me</h3>
+      </div>
+
+      <div className="section-body grid-2">
+        <div className="column-1">
+          <h3 className="title-sm">Hello, I'm</h3>
+          <p className="text">
+            Hi! I`m Elijah Nyasiando. A Front end and back end Developer (full stack developer)
+          </p>
+          <div className="skills">
+            <div className="skill html">
+              <h3 className="skill-title">HTML</h3>
+              <div className="skill-bar">
+                <div className="skill-progress" data-progress="90%"></div>
+              </div>
+            </div>
+            <div className="skill css">
+              <h3 className="skill-title">CSS</h3>
+              <div className="skill-bar">
+                <div className="skill-progress" data-progress="70%"></div>
+              </div>
+            </div>
+            <div className="skill js">
+              <h3 className="skill-title">JavaScript</h3>
+              <div className="skill-bar">
+                <div className="skill-progress" data-progress="80%"></div>
+              </div>
+            </div>
+          </div>
+          <a href="" className="btn">Read more</a>
+        </div>
+
+        <div className="column-2 image">
+          
+        </div>
+      </div>
+    </div>
+  </section>
   )
 }
--- mgk_creates/src/components/ContactUs.js
+++ mgk_creates/src/components/ContactUs.js
@@ -2,6 +2,72 @@ import React from 'react'
 
 export default function ContactUs() {
   return (
-    <div>Country</div>
+    <div>
+    <section class="contact" id="contact">
+                <div className="container">
+                  <div class="contact-box">
+                    <div className="contact-info">
+                      <h3 className="title">Get in touch</h3>
+                      <p className="text">
+                        lets talk!
+                      </p>
+                      <div className="information-wrap">
+                        <div className="information">
+                          <div className="contact-icon">
+                            <i className="fas fa-map-marker-alt"></i>
+                          </div>
+                          <p className="info-text">Nairobi, Kenya</p>
+                        </div>
+        
+                        <div className="information">
+                          <div className="contact-icon">
+                            <i className="fas fa-paper-plane"></i>
+                          </div>
+                          <p class="info-text">elijah.nyasiando.mn@gmail.com</p>
+                        </div>
+        
+                        <div className="information">
+                          <div className="contact-icon">
+                            <i className="fas fa-phone-alt"></i>
+                          </div>
+                          <p className="info-text">+2547-1442-4586</p>
+                        </div>
+                      </div>
+                    </div>
+        
+                    <div className="contact-form">
+                      <h3 className="title">Contact me</h3>
+                      <div className="row">
+                        <input type="text" className="contact-input" placeholder="First Name"/>
+                        <input type="text" className="contact-input" placeholder="Last Name"/>
+                      </div>
+        
+                      <div class="row">
+                        <input type="text" className="contact-input" placeholder="Phone" />
+                        <input type="email" className="contact-input" placeholder="Email" />
+                      </div>
+        
+                      <div class="row">
+                        <textarea name="message" className="contact-input textarea" placeholder="Message"></textarea>
+                      </div>
+                      <a href=" " className="btn">Send</a>
+                    </div>
+                  </div>
+                </div>
+            </section>
+
+            <section className="hireme" id="hireme">
+                <div className="container">
+                  <h3 className="title">Let's talk about a project</h3>
+                  <p className="text">
+                    Hey! Do you wish to collaborate on a project? Do you have an idea that you want it to be realised?
+                     <br/>
+                    Ellicit my services. You will be amazed on what can be done.
+
+                  </p>
+                  <a href="" className="btn">Hire me</a>
+                </div>
+            </section>
+    </div>
   )
 }
